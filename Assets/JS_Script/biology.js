ae05a8ee2b9e711154ae90a8bb3e81775b310427
sakmor <sakmor@gmail.com>
2016-07-08 17:10:39 +0800

    新增biology Script

--- Assets/JS_Script/biology.js
+++ Assets/JS_Script/biology.js
@@ -1,6 +1,54 @@
 // #pragma strict
-var k = "apple k";
+var speed: float;
+var moveSpeed: float;
+var rotateSpeed: float;
+var bioAction: String;
+var Sphere: GameObject;
 
-function test() {
-    return k;
+function Start() {
+
+    moveSpeed = moveSpeed || 0.01;
+    speed = speed || 8;
+}
+
+function Update() {
+    this._movment();
+    this._animations();
+
+}
+
+function _animations() {
+    //撠??????拇??????嚗??剜?曉?????雿?
+    var nowAnimation: AnimationState;
+    switch (this.bioAction) {
+    case "attack":
+        this.GetComponent. < Animation > ().Play("Attack");
+        break;
+    case "hurt":
+        break;
+    case "Walk":
+        this.GetComponent. < Animation > ().Play("Walk");
+        break;
+    case "picking":
+        break;
+    case "Wait":
+        this.GetComponent. < Animation > ().Play("Wait");
+        break;
+    }
+}
+
+function _movment() {
+    //將生物移動向目標
+    this.transform.position = Vector3.MoveTowards(this.transform.position, Sphere.transform.position, moveSpeed);
+    if (Vector3.Distance(this.transform.position, Sphere.transform.position) > 1) {
+        this.bioAction = "Walk";
+    } else {
+        this.bioAction = "Wait";
+    }
+
+    //將生物轉向目標
+    var targetDir = Sphere.transform.position - this.transform.position;
+    var step = speed * Time.deltaTime;
+    var newDir = Vector3.RotateTowards(this.transform.forward, targetDir, step, 0.0);
+    this.transform.rotation = Quaternion.LookRotation(newDir);
 }
--- Assets/JS_Script/game.js
+++ Assets/JS_Script/game.js
@@ -1,91 +1,31 @@
 // # pragma strict
-var player: GameObject;
 var Plane: GameObject;
-var apple: GameObject;
-
-//轉向目標使用
-var speed: float;
-var moveSpeed: float;
+var Sphere: GameObject;
+var Player: GameObject;
 
 function Start() {
-    player = GameObject.Find("Cha_Knight");
+
     Plane = GameObject.Find("Plane");
-    apple = GameObject.Find("Sphere");
-    speed = 8;
-    moveSpeed = 0.02;
+    Sphere = GameObject.Find("Sphere");
+    Player = GameObject.Find("Cha_Knight");
+    Player.AddComponent(biology);
+    Player.GetComponent(biology).Sphere = Sphere;
 
 }
 
 function Update() {
-    contorlMangers();
-
+    getMousehitGroupPos();
 
 }
+//========================================================
 
-
-/**
- * contorlMangers -輸入管理員
- * todo:
- * 	之後要獨立於另一個JS，並改為點擊地面移動
- * @return {[string]} keydown [return user input action]
- */
-function contorlMangers() {
-
+function getMousehitGroupPos() {
     //滑鼠點擊取得做標點
     var mouseHitPlane: RaycastHit;
     var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
     if (Physics.Raycast(ray, mouseHitPlane) && mouseHitPlane.transform.tag == "ground") {
         if (Input.GetMouseButtonDown(0)) {
-            apple.transform.position = mouseHitPlane.point;
+            Sphere.transform.position = mouseHitPlane.point;
         }
     }
-
-
-    //將生物轉向目標
-    var targetDir = apple.transform.position - player.transform.position;
-    var step = speed * Time.deltaTime;
-    var newDir = Vector3.RotateTowards(player.transform.forward, targetDir, step, 0.0);
-    player.transform.rotation = Quaternion.LookRotation(newDir);
-
-    //將生物移動向目標
-    player.transform.position = Vector3.MoveTowards(player.transform.position, apple.transform.position, moveSpeed);
-    if (Vector3.Distance(player.transform.position, apple.transform.position) > 1) {
-        player.GetComponent. < Animation > ().Play("Walk");
-    }
-
-    //使用WASD與上下左右鍵移動角色
-    var keydown = "";
-    if (Input.anyKey) {
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
-
-            player.transform.Translate(0, 0, 1 * Time.deltaTime);
-
-            keydown = "User typing W / UpArrow";
-        }
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
-            player.transform.Translate(0, 0, -1 * Time.deltaTime);
-            keydown = "User typing S / DownArrow";
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
-            player.transform.Translate(-1 * Time.deltaTime, 0, 0);
-            keydown = "User typing A / LeftArrow";
-        }
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
-            player.transform.Translate(1 * Time.deltaTime, 0, 0);
-            keydown = "User typing D / RightArrow";
-        }
-        if (Input.GetKey(KeyCode.Space)) {
-            apple.transform.position = player.transform.position;
-            print("space");
-            player.GetComponent. < Animation > ().Play("Attack");
-            //            player.GetComponent. < Animation > ().PlayQueued("wait");
-        } else {
-
-        }
-    } else {
-
-
-    }
-    return keydown;
 }
