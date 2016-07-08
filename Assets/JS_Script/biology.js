fc8f6d6cd26ca94bab2903b92b7546c8ac48b3b9
sakmor <sakmor@gmail.com>
2016-07-08 20:20:31 +0800

    接下來該加入minecraft功能了

--- Assets/JS_Script/biology.js
+++ Assets/JS_Script/biology.js
@@ -1,54 +1,73 @@
 // #pragma strict
-var speed: float;
 var moveSpeed: float;
+var moveSpeedMax: float;
 var rotateSpeed: float;
+var rotateSpeedMax: float;
 var bioAction: String;
 var Sphere: GameObject;
+var anim: Animation;
+var WalkSteptweek: float;
+
 
 function Start() {
+    WalkSteptweek = WalkSteptweek || 50;
+    moveSpeed = moveSpeed || 0.1;
+    moveSpeedMax = moveSpeed;
+    rotateSpeed = rotateSpeed || 10;
+    rotateSpeedMax = rotateSpeed;
+    anim = this.GetComponent. < Animation > ();
 
-    moveSpeed = moveSpeed || 0.01;
-    speed = speed || 8;
 }
 
 function Update() {
     this._movment();
     this._animations();
-
 }
 
+
+
 function _animations() {
-    //撠??????拇??????嚗??剜?曉?????雿?
-    var nowAnimation: AnimationState;
+    //對應生物所處狀態，播放對應動作
     switch (this.bioAction) {
     case "attack":
-        this.GetComponent. < Animation > ().Play("Attack");
         break;
     case "hurt":
         break;
     case "Walk":
-        this.GetComponent. < Animation > ().Play("Walk");
+        anim.CrossFade("Walk");
         break;
     case "picking":
         break;
     case "Wait":
-        this.GetComponent. < Animation > ().Play("Wait");
+        anim.CrossFade("Wait");
         break;
     }
 }
 
 function _movment() {
     //將生物移動向目標
-    this.transform.position = Vector3.MoveTowards(this.transform.position, Sphere.transform.position, moveSpeed);
-    if (Vector3.Distance(this.transform.position, Sphere.transform.position) > 1) {
+    if (Vector3.Distance(this.transform.position, Sphere.transform.position) > 0.5) {
+        moveSpeed = moveSpeedMax;
         this.bioAction = "Walk";
+        //依照目標距離調整移動速度
+        if (Vector3.Distance(this.transform.position, Sphere.transform.position) < 1.5) {
+            moveSpeed = moveSpeed * (Vector3.Distance(this.transform.position, Sphere.transform.position) / 1.5);
+        }
     } else {
         this.bioAction = "Wait";
     }
 
+    //移動生物到目標點
+    this.transform.position = Vector3.MoveTowards(this.transform.position, Sphere.transform.position, moveSpeed);
+
+    //調整步伐
+    anim["Walk"].speed = WalkSteptweek * moveSpeed;
+
     //將生物轉向目標
     var targetDir = Sphere.transform.position - this.transform.position;
-    var step = speed * Time.deltaTime;
+    var step = rotateSpeed * Time.deltaTime;
     var newDir = Vector3.RotateTowards(this.transform.forward, targetDir, step, 0.0);
     this.transform.rotation = Quaternion.LookRotation(newDir);
+
+    //    print(Vector3.Angle(targetDir, this.transform.forward));
 }
--- Assets/JS_Script/game.js
+++ Assets/JS_Script/game.js
@@ -2,22 +2,57 @@
 var Plane: GameObject;
 var Sphere: GameObject;
 var Player: GameObject;
+var PlayerLight: GameObject;
+var PlayerCamera: GameObject;
 
 function Start() {
-
+    PlayerLight = GameObject.Find("PlayerLight");
     Plane = GameObject.Find("Plane");
     Sphere = GameObject.Find("Sphere");
     Player = GameObject.Find("Cha_Knight");
+    PlayerCamera = GameObject.Find("PlayerCamera");
+    Sphere.transform.position = Player.transform.position;
     Player.AddComponent(biology);
     Player.GetComponent(biology).Sphere = Sphere;
+
+
 
 }
 
 function Update() {
     getMousehitGroupPos();
+    fellowPlayerLight();
+    fellowPlayerCameraMove();
+    fellowPlayerCameraContorl();
 
 }
 //========================================================
+
+
+
+
+function fellowPlayerCameraMove() {
+    PlayerCamera.transform.position.x = Player.transform.position.x + -12;
+    PlayerCamera.transform.position.z = Player.transform.position.z + -12;
+
+}
+
+function fellowPlayerCameraContorl() {
+    if (Input.GetAxis("Mouse ScrollWheel") < 0) // forward
+    {
+        if (Camera.main.orthographicSize > 1)
+            Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize - 1, 10);
+    }
+
+    if (Input.GetAxis("Mouse ScrollWheel") > 0) // forward
+    {
+        Camera.main.orthographicSize = Mathf.Min(Camera.main.orthographicSize + 1, 10);
+    }
+}
+
+function fellowPlayerLight() {
+    PlayerLight.transform.position = Vector3(Player.transform.position.x, PlayerLight.transform.position.y, Player.transform.position.z);
+}
 
 function getMousehitGroupPos() {
     //滑鼠點擊取得做標點
