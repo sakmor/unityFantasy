6396e0a80cc7082cedc5386597b575e2e089f46e
王山姆 <sakmor@gmail.com>
2017-01-02 23:27:16 +0800

    開始撰寫gameCS

---
+++ Assets/Scripts/gameCS.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameCS : MonoBehaviour
+{
+    //GameObject
+    GameObject Player, Cube, mainCamera, mainCamera2, hitUIObject, movePlateMouse, movePlate, cammeraPlate, logText;
+
+    //Dictionary、Array----------------------------
+    int[] cubeArrayTxt;
+
+    Dictionary<Vector3, Vector2> cubesPosDictionary =
+               new Dictionary<Vector3, Vector2>();
+
+    //boolean----------------------------
+    bool cammeraPlatein2out, movePlatein2out, clickStart, touchScreen;
+
+    //String----------------------------
+    string hitUIObjectName = "";
+
+    //Vector2、Vector3----------------------------
+    Vector2 myIputPostion;
+    Vector3 mouseStartPOS, mouseDragVector, cameraRELtarget;
+
+    //Script 自定義----------------------------
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
