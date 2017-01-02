2ad700ea1ff67616c38de41c16e3401b88dcd57f
王山姆 <sakmor@gmail.com>
2017-01-03 00:08:53 +0800

    持續改gameCS

--- Assets/Scripts/gameCS.cs
+++ Assets/Scripts/gameCS.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class gameCS : MonoBehaviour
 {
     //GameObject
-    GameObject Player, Cube, mainCamera, mainCamera2, hitUIObject, movePlateMouse, movePlate, cammeraPlate, logText;
+    GameObject cammeraPlateMouse, Player, Cube, mainCamera, mainCamera2, hitUIObject, movePlateMouse, movePlate, cammeraPlate, logText;
 
     //Dictionary、Array----------------------------
     int[] cubeArrayTxt;
-
+    GameObject[] allBiologys;
     Dictionary<Vector3, Vector2> cubesPosDictionary =
                new Dictionary<Vector3, Vector2>();
 
@@ -21,14 +21,38 @@ public class gameCS : MonoBehaviour
 
     //Vector2、Vector3----------------------------
     Vector2 myIputPostion;
-    Vector3 mouseStartPOS, mouseDragVector, cameraRELtarget;
+    Vector3 lastCameraPos, mouseStartPOS, mouseDragVector, cameraRELtarget;
 
     //Script 自定義----------------------------
+    Pathfinding PathfindingCS;
+    biologyCS playerBioCS;
+    TextAsset biologyList;
 
+    //UnityEngine ----------------------------
+    Camera camera1, camera2;
     // Use this for initialization
     void Start()
     {
+        //將所有生物套上biologyCS;
+        allBiologys = GameObject.FindGameObjectsWithTag("biology");
+        foreach (GameObject thisBiology in allBiologys)
+        {
+            thisBiology.AddComponent<biologyCS>();
+        }
 
+        biologyList = Resources.Load("db/biologyList", typeof(TextAsset)) as TextAsset;
+        logText = GameObject.Find("logText");
+        // logg('This Device is:' + SystemInfo.deviceType);
+
+        cammeraPlateMouse = GameObject.Find("cammeraPlateMouse");
+        movePlateMouse = GameObject.Find("movePlateMouse");
+        movePlate = GameObject.Find("movePlate");
+        Cube = GameObject.Find("Cube");
+        Player = GameObject.Find("Cha_Knight");
+        mainCamera = GameObject.Find("mainCamera");
+        mainCamera2 = GameObject.Find("mainCamera2");
+        playerBioCS = Player.GetComponent<biologyCS>();
+        cammeraPlate = GameObject.Find("cammeraPlate");
     }
 
     // Update is called once per frame
@@ -36,4 +60,6 @@ public class gameCS : MonoBehaviour
     {
 
     }
+
+
 }
