ba06b462ddc8829820fe730d2bd0b87ff9ad3bfc
sakmor <sakmor@gmail.com>
2017-01-19 13:26:39 +0800

    新增小行星的偏移座標計算方式

--- Assets/nodeInfo.cs
+++ Assets/nodeInfo.cs
@@ -19,16 +19,28 @@ public class nodeInfo : MonoBehaviour
     public Node parent;
     public Grid grid;
     public float dregg;
+    public float bais;
     public Transform target;
     void Start()
     {
+        target = GameObject.Find("Cha_Knight").transform;
         grid = GameObject.Find("aStart").GetComponent<Grid>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        r = Vector3.Distance(target.position, GameObject.Find("nodeINFO (1)").transform.position);
+        // dregg = Vector3.Angle(new Vector3(1, 0, 0), GameObject.Find("nodeINFO (1)").transform.position);
+        Vector3 cross = Vector3.Cross(new Vector3(1, 0, 0), GameObject.Find("nodeINFO (1)").transform.position);
+        // if (cross.z > 0)
+        // {
+        //     dregg = 360 - dregg;
+        // }
+
         //circle arow around targt
+        dregg = AngleBetweenVector3(target.position, GameObject.Find("nodeINFO (1)").transform.position) + bais;
         this.transform.position = target.position + r * (new Vector3(Mathf.Cos(dregg * Mathf.Deg2Rad), 0, Mathf.Sin(dregg * Mathf.Deg2Rad)));
 
         playerDist = Vector3.Distance(GameObject.Find("Cha_Knight").transform.position, this.transform.position);
@@ -38,5 +50,12 @@ public class nodeInfo : MonoBehaviour
         worldPosition = grid.NodeFromWorldPoint(this.transform.position).worldPosition;
         mPosition = GameObject.Find("mainGame").GetComponent<gameCS>().normalized(worldPosition);
 
+    }
+
+    private float AngleBetweenVector3(Vector3 vec1, Vector3 vec2)
+    {
+        Vector3 diference = vec2 - vec1;
+        float sign = (vec2.z < vec1.z) ? -1.0f : 1.0f;
+        return Vector3.Angle(Vector2.right, diference) * sign;
     }
 }
